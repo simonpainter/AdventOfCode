d91ea7ee71361d46a653d719a33db6beb82bdf9e
simon.painter <simon.painter@gmail.com>
2017-12-01 19:43:12 +0000

    Second star on day 1 completed

--- 1/code.py
+++ 1/code.py
@@ -1,9 +1,13 @@
-output = []
+part1 = []
+part2 = []
 with open ("input.txt", "r") as inputfile:
 	data=inputfile.readlines()
 	string = data[0].strip()
 for i in range(0, len(string)):
 	if string[i]==string[i-1]:
-		output.append(int(string[i]))
-s=sum(output)
-print(s)
+		part1.append(int(string[i]))
+	if string[i]==string[i-(len(string)/2)]:
+		part2.append(int(string[i]))
+
+print(sum(part1))
+print(sum(part2))
