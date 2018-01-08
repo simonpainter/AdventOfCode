3daf315eab1e601360ea91670523071e2197109b
simon.painter <simon.painter@gmail.com>
2018-01-08 23:25:53 +0000

    Finished 19

--- 2017/19/code1.py
+++ 2017/19/code1.py
@@ -1,22 +1,62 @@
 y = 0
-directionXY = 0,+1
+count = 0
+north = 0,-1
+south = 0,1
+east =  1,0
+west = -1,0
 
 
-def move(x,y, directionXY):
-	x += directionXY[0]
-	y += directionXY[1]
+directionXY = south
+direction = "|"
+sequence = []
+def move(_x,_y, _directionXY):
+	global count
+	_x += _directionXY[0]
+	_y += _directionXY[1]
+	count +=1
+	return _x,_y
 
 
-	return x,y
+def look(_x,_y,cardinal):
+	_x+=cardinal[0]
+	_y+=cardinal[1]
+	return _x,_y
 
-
-with open ("testinput.txt", "r") as inputfile:
+with open ("input.txt", "r") as inputfile:
         map=inputfile.readlines()
 	x = map[y].index("|")
 	while map[y][x]!=" ":
-		print map[y][x]
-		x,y = move(x,y,directionXY)
+		if map[y][x] == "|" or map[y][x] == "-":
+			x,y = move(x,y,directionXY)
+		elif map[y][x] == "+":
+			print("turn")
+			if direction == "|":
+				direction = "-"
+				tmpx,tmpy = look(x,y,east)
+				if tmpy < len(map) and tmpx < len(map[tmpy]):
+					if map[tmpy][tmpx] != " ":
+						directionXY=east
+				tmpx,tmpy = look(x,y,west)
+				if tmpy < len(map) and tmpx < len(map[tmpy]):
+					if map[tmpy][tmpx] != " ":
+						directionXY=west
+			elif direction == "-":
+				direction = "|"
+				tmpx,tmpy = look(x,y,north)
+				if tmpy < len(map) and tmpx < len(map[tmpy]):
+					if map[tmpy][tmpx] != " ":
+						directionXY=north
+				tmpx,tmpy = look(x,y,south)
+				if tmpy < len(map) and tmpx < len(map[tmpy]):
+					if map[tmpy][tmpx] != " ":
+						directionXY=south
+			x,y = move(x,y,directionXY)
+		else:
+			sequence.append(map[y][x])
+			x,y = move(x,y,directionXY)
+
+		print(map[y][x])
 
 
-
-
+	print("".join(sequence))
+	print(count)
