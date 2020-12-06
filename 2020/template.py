16efe50a19d0d0e7c6884a3eaab8abb78ce3567f
Simon Painter <simon.painter@gmail.com>
2020-12-06 12:31:31 +0000

    Started using a template with some common code

---
+++ 2020/template.py
@@ -0,0 +1,15 @@
+import re, itertools
+
+with open ("testinput.txt", "r") as inputfile:
+	data = inputfile.read()
+	lines = data.split('\n')
+	groups = data.split'\n\n')
+
+def part1(input)
+	pass
+
+def part2(input)
+	pass
+
+print(part1(data))
+print(part2(data))
