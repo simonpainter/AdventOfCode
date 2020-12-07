8e0b7c2a5d871c619bf868d9f9fe7c4cd5be926a
Simon Painter <simon.painter@gmail.com>
2020-12-07 21:40:54 +0000

    Learned a lot about graphs

--- 2020/7/code.py
+++ 2020/7/code.py
@@ -1,13 +1,41 @@
-with open ("testinput.txt", "r") as inputfile:
+import re, itertools
+import networkx as nx
+
+with open ("input.txt", "r") as inputfile:
 	data = inputfile.read()
 	lines = data.split('\n')
-	groups = data.split'\n\n')
+	groups = data.split('\n\n')
 
-def part1(input)
-	pass
+input = []
+for line in lines:
+	contentlist = {}
 
-def part2(input)
-	pass
+	bag = line.split(' bags contain')
+	for contents in bag[1].split(","):
+		r = re.findall('(\d|no) (.*) bag',contents.strip('.').strip(' '))
+		if r[0][0] != 'no':
+			contentlist[r[0][1]] = r[0][0]
+	appendlist = bag[0],contentlist
+	input.append(appendlist)
 
-print(part1(data))
-print(part2(data))
+
+def part1(input):
+	bags = nx.DiGraph()
+	for bag in input:
+		for contents in bag[1]:
+			bags.add_edge(bag[0],contents)
+	return len(nx.ancestors(bags,'shiny gold'))
+def part2(input):
+	bags = nx.DiGraph()
+	for bag in input:
+		for contents in bag[1]:
+			bags.add_edge(bag[0],contents,  weight=int(bag[1][contents]))
+	def dfs_cost(node):
+		cost = 0
+		for n, weight in bags[node].items():
+			cost = cost + weight['weight'] + weight['weight'] * dfs_cost(n)
+		return cost
+	return dfs_cost('shiny gold')
+
+print(part1(input))
+print(part2(input))
--- 2020/template.py
+++ 2020/template.py
@@ -3,12 +3,12 @@ import re, itertools
 with open ("testinput.txt", "r") as inputfile:
 	data = inputfile.read()
 	lines = data.split('\n')
-	groups = data.split'\n\n')
+	groups = data.split('\n\n')
 
-def part1(input)
+def part1(input):
 	pass
 
-def part2(input)
+def part2(input):
 	pass
 
 print(part1(data))
