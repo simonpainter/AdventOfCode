8e14fb6e1b01e6332e734743224ac0528b1d761c
Simon Painter <simon.painter@gmail.com>
2022-02-21 22:08:10 +0000

    Added the test cases.

--- 2021/2/day2.py
+++ 2021/2/day2.py
@@ -1,17 +1,19 @@
 import re, itertools, math
 
-with open ("input.txt", "r") as inputfile:
-	data = inputfile.read()
-	lines = data.split('\n')
-	groups = data.split('\n\n')
+def parse_input(path):
+	with open (path, "r") as inputfile:
+		data = inputfile.read()
+		lines = data.split('\n')
+		groups = data.split('\n\n')
 
-input = []
-for line in lines:
-	#input.append(int(line))
-	input.append(line.split(' '))
-#for group in groups:
-	#input.append(group.replace("\n", "")
-	#input.append(group.split(' '))
+	input = []
+	for line in lines:
+		#input.append(int(line))
+		input.append(line.split(' '))
+	#for group in groups:
+		#input.append(group.replace("\n", "")
+		#input.append(group.split(' '))
+	return input
 
 def part1(input):
 	distance = 0
@@ -39,5 +41,22 @@ def part2(input):
 			aim -= int(command[1])
 	return distance * depth
 
-print(part1(input))
-print(part2(input))
+testresult_part1 = 150
+testresult_part2 = 900
+
+
+if testresult_part1 == part1(parse_input("testinput.txt")):
+	print("*** Part 1 Test Passed ***")
+	print("----Part 1 Result ", part1(parse_input("input.txt")))
+else:
+	print("Part 1 Test Failed")
+	print("Part 1 Test Result", part1(parse_input("testinput.txt")))
+	print("Expected ", testresult_part1)
+	
+if testresult_part2 == part2(parse_input("testinput.txt")):
+	print("*** Part 2 Test Passed ***")
+	print("----Part 2 Result ", part2(parse_input("input.txt")))
+else:
+	print("Part 2 Test Failed")
+	print("Part 2 Test Result", part2(parse_input("testinput.txt")))
+	print("Expected ", testresult_part2)
