0c379d9748d88a650682e9994d04ff7025661725
Simon Painter <simon.painter@gmail.com>
2022-12-01 10:49:32 +0000

    Day 1 2022

---
+++ 2022/1/day1.py
@@ -0,0 +1,58 @@
+import re, itertools, math,os,sys
+
+           
+pathname = os.path.dirname(sys.argv[0])        
+testinput_path = pathname + "/testinput.txt"   
+input_path = pathname + "/input.txt"
+
+
+
+def parse_input(path):
+	with open (path, "r") as inputfile:
+		data = inputfile.read()
+		lines = data.split('\n')
+		groups = data.split('\n\n')
+
+	input = []
+	#for line in lines:
+		#input.append(int(line))
+		#input.append(line.split(' '))
+	for group in groups:
+		lines = group.split('\n')
+		elf = []
+		for item in lines:
+			elf.append(int(item))
+		input.append(elf)
+	return input
+
+def part1(input):
+	totals = []
+	for elf in input:
+		totals.append(sum(elf))
+	return max(totals)
+
+def part2(input):
+	totals = []
+	for elf in input:
+		totals.append(sum(elf))
+	totals.sort()
+	return sum(totals[-3:])
+
+testresult_part1 = 24000
+testresult_part2 = 45000
+
+if testresult_part1 == part1(parse_input(testinput_path)):
+	print("*** Part 1 Test Passed ***")
+	print("----Part 1 Result ", part1(parse_input(input_path)))
+else:
+	print("Part 1 Test Failed")
+	print("Part 1 Test Result", part1(parse_input(testinput_path)))
+	print("Expected ", testresult_part1)
+	
+if testresult_part2 == part2(parse_input(testinput_path)):
+	print("*** Part 2 Test Passed ***")
+	print("----Part 2 Result ", part2(parse_input(input_path)))
+else:
+	print("Part 2 Test Failed")
+	print("Part 2 Test Result", part2(parse_input(testinput_path)))
+	print("Expected ", testresult_part2)
