2a0426c05fd4453bc9958a3f6a9a8d1c355bd74f
Simon Painter <simon.painter@gmail.com>
2022-12-03 23:37:08 +0000

    Cleaned it up a bit.

--- 2022/3/day3.py
+++ 2022/3/day3.py
@@ -13,43 +13,26 @@ def parse_input(path):
 		data = inputfile.read()
 		lines = data.split('\n')
 		groups = data.split('\n\n')
+	return lines
 
-	input = lines
-	#for line in lines:
-		#input.append(int(line))
-		#input.append(line.split(' '))
-	#for group in groups:
-		#input.append(group.replace("\n", "")
-		#input.append(group.split(' '))
-	return input
 def part1(input):
 	anomalies = []
-	total = 0
 	for bag in input:
 		compartments = [bag[:len(bag)//2],bag[len(bag)//2:]]
 		result = ''
-		for char in compartments[0]:
-			if char in compartments[1] and not char in result:
-				result+=char
-		anomalies.append(result)
-	for each in anomalies:
-		total += (string.ascii_letters.find(each) + 1)
-	return total
-
-
+		result = set(compartments[0]) & set(compartments[1])
+		anomalies.append(result.pop())
+	return sum([(string.ascii_letters.find(each) + 1) for each in anomalies])
 
 def part2(input):
 	elf_groups = []
 	badges = []
-	total = 0
 	for i in range(0, len(input), 3):
 		elf_groups.append(input[i:i + 3])
 	for elf_group in elf_groups:
 		common = set.intersection(*map(set,elf_group))
-		badges.append(list(common)[0])
-	for each in badges:
-		total += (string.ascii_letters.find(each) + 1)
-	return total		
+		badges.append(common.pop())
+	return sum([(string.ascii_letters.find(each) + 1) for each in badges])
 
 
 
@@ -75,7 +58,7 @@ if testresult_part2 == part2(parse_input(testinput_path)):
 	start = time.time()
 	print("----Part 2 Result: ", part2(parse_input(input_path)))
 	end = time.time()
-	print("====Part 1 Time ", end - start)
+	print("====Part 2 Time ", end - start)
 
 else:
 	print("Part 2 Test Failed")
