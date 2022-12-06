aad84cf641656e10cb76f9aa62b1cfe813d73a0b
Simon Painter <simon.painter@gmail.com>
2022-12-06 10:48:25 +0000

    More bloody sets

---
+++ 2022/6/day6.py
@@ -0,0 +1,61 @@
+import re, itertools, math,os,sys,string,time
+
+           
+pathname = os.path.dirname(sys.argv[0])        
+testinput_path = pathname + "/testinput.txt"   
+input_path = pathname + "/input.txt"
+
+
+
+def parse_input(path):
+	with open (path, "r") as inputfile:
+		data = inputfile.read()
+		lines = data.split('\n')
+		groups = data.split('\n\n')
+
+	input = data
+	#for line in lines:
+		#input.append(int(line))
+		#input.append(line.split(' '))
+	#for group in groups:
+		#input.append(group.replace("\n", "")
+		#input.append(group.split(' '))
+	return input
+
+def part1(input):
+	for i in range (4,len(input)+1):
+		if list(sorted(set(input[i-4:i])))==list(sorted(input[i-4:i])):
+			return i
+
+def part2(input):
+	for i in range (14,len(input)+1):
+		if list(sorted(set(input[i-14:i])))==list(sorted(input[i-14:i])):
+			return i
+
+testresult_part1 = 5
+testresult_part2 = 19
+
+
+if testresult_part1 == part1(parse_input(testinput_path)):
+	print("*** Part 1 Test Passed ***")
+	start = time.time()
+	print("----Part 1 Result: ", part1(parse_input(input_path)))
+	end = time.time()
+	print("====Part 1 Time ", end - start)
+
+else:
+	print("Part 1 Test Failed")
+	print("Part 1 Test Result: ", part1(parse_input(testinput_path)))
+	print("Expected ", testresult_part1)
+	
+if testresult_part2 == part2(parse_input(testinput_path)):
+	print("*** Part 2 Test Passed ***")
+	start = time.time()
+	print("----Part 2 Result: ", part2(parse_input(input_path)))
+	end = time.time()
+	print("====Part 2 Time ", end - start)
+
+else:
+	print("Part 2 Test Failed")
+	print("Part 2 Test Result: ", part2(parse_input(testinput_path)))
+	print("Expected ", testresult_part2)
