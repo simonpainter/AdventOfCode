925382a208aaf9a1c80f23373bd4ab42a96f357e
Simon Painter <simon.painter@gmail.com>
2024-12-17 14:10:07 +0000

    Changed it to use Andy's method.

--- 2024/17/day17.py
+++ 2024/17/day17.py
@@ -1,5 +1,7 @@
-import re, itertools, math,os,sys,string,time
-
+import re, itertools, math, os, sys, string, time
+from collections import defaultdict
+import heapq
+           
 pathname = os.path.dirname(sys.argv[0])        
 testinput_path1 = pathname + "/testinput1.txt"   
 testinput_path2 = pathname + "/testinput2.txt"  
@@ -20,27 +22,21 @@ def run_program(program, a_val, b_val, c_val):
     A = a_val
     B = b_val
     C = c_val
-    
     if isinstance(program, str):
         program = [int(x) for x in program.split(',')]
-    
     outputs = []
     ip = 0
-    
     while ip < len(program):
         if ip + 1 >= len(program):
             break
-            
         opcode = program[ip]
         operand = program[ip + 1]
-        
         def get_combo_value(op):
             if op <= 3: return op
             elif op == 4: return A
             elif op == 5: return B
             elif op == 6: return C
             return 0
-        
         if opcode == 0: A = A // (2 ** get_combo_value(operand))
         elif opcode == 1: B = B ^ operand
         elif opcode == 2: B = get_combo_value(operand) % 8
@@ -52,33 +48,8 @@ def run_program(program, a_val, b_val, c_val):
         elif opcode == 5: outputs.append(str(get_combo_value(operand) % 8))
         elif opcode == 6: B = A // (2 ** get_combo_value(operand))
         elif opcode == 7: C = A // (2 ** get_combo_value(operand))
-            
         ip += 2
-    
     return ','.join(outputs)
-
-def find_next_value(target, program, prev_value, prev_diff,registers):
-    if prev_diff == 0:
-        base = prev_value + 1
-        predicted = base + 1000
-    else:
-        predicted = prev_value + (prev_diff * 8)
-        base = max(prev_value + 1, predicted - 1000)
-    
-    while True:
-        output = run_program(program, base, registers['B'], registers['C'])
-        if output.endswith(target):
-            return base
-        base += 1
-        if base > predicted + 1000:
-            break
-    
-    base = prev_value
-    while True:
-        base += 1
-        output = run_program(program, base, registers['B'], registers['C'])
-        if output.endswith(target):
-            return base
 
 def part1(input):
     registers, program = input
@@ -86,24 +57,27 @@ def part1(input):
 
 def part2(input):
     registers, program = input
-    current_target = "3,0"
-    current_a = find_next_value(current_target, program, 0, 0,registers)
+    target_digits = program.split(',')
     
-    target_parts = program.split(',')
-    current_length = 2
-    prev_value = current_a
-    prev_diff = 0
+    a = 1
+    match_length = 1
     
-    while current_length < len(target_parts):
-        next_digit = target_parts[-(current_length + 1)]
-        current_target = next_digit + "," + current_target
-        new_value = find_next_value(current_target, program, prev_value, prev_diff,registers)
-        prev_diff = new_value - prev_value
-        prev_value = new_value
-        current_a = new_value
-        current_length += 1
+    while match_length <= len(target_digits):
+        output = run_program(program, a, 0, 0)
+        output_digits = output.split(',')
         
-    return current_a
+        if output_digits[-match_length:] == target_digits[-match_length:]:
+            if match_length < len(target_digits):
+                a *= 8
+                match_length += 1
+            else:
+                return a
+        else:
+            a += 1
+            
+    return a
+
+
 
 testresult_part1 = "4,6,3,5,6,3,5,2,1,0"
 testresult_part2 = 117440
