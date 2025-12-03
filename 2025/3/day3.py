49d4773ba2e1229df80a633dbe1499a1884761c3
Simon Painter <simon.painter@gmail.com>
2025-12-03 15:21:23 +0000

    Part 1 done with max loop

---
+++ 2025/3/day3.py
@@ -0,0 +1,67 @@
+import re, itertools, math, os, sys, string, time
+import collections, heapq, functools
+
+           
+pathname = os.path.dirname(sys.argv[0])        
+testinput_path1 = pathname + "/testinput1.txt"   
+testinput_path2 = pathname + "/testinput2.txt"  
+input_path = pathname + "/input.txt"
+
+
+
+def parse_input(path):
+	with open (path, "r") as inputfile:
+		data = inputfile.read()
+		lines = data.split('\n')
+		groups = data.split('\n\n')
+
+	input = []
+	for line in lines:
+		input.append(str(line))
+		#input.append(line.split(' '))
+	#for group in groups:
+		#input.append(group.replace("\n", "")
+		#input.append(group.split(' '))
+	return input
+
+def part1(input):
+	total = 0
+	formatted = []
+	for line in input:
+		list_line = list(map(int,list(line)))
+		formatted.append(list_line)
+		tens = max(list_line[:-1])
+		units = max(list_line[list_line.index(tens)+1:])
+		total += int(str(tens) + str(units))
+	return total
+
+def part2(input):
+	pass
+
+testresult_part1 = 357
+testresult_part2 = 0
+
+
+if testresult_part1 == part1(parse_input(testinput_path1)):
+	print("*** Part 1 Test Passed ***")
+	start = time.time()
+	print("----Part 1 Result: ", part1(parse_input(input_path)))
+	end = time.time()
+	print("====Part 1 Time ", end - start)
+
+else:
+	print("Part 1 Test Failed")
+	print("Part 1 Test Result: ", part1(parse_input(testinput_path1)))
+	print("Expected ", testresult_part1)
+	
+if testresult_part2 == part2(parse_input(testinput_path2)):
+	print("*** Part 2 Test Passed ***")
+	start = time.time()
+	print("----Part 2 Result: ", part2(parse_input(input_path)))
+	end = time.time()
+	print("====Part 2 Time ", end - start)
+
+else:
+	print("Part 2 Test Failed")
+	print("Part 2 Test Result: ", part2(parse_input(testinput_path2)))
+	print("Expected ", testresult_part2)
